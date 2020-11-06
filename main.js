c8cf6b6b3b71a7e68e4778833ce3c1457c07c0b4
KnlnKS <soulkks@hotmail.ca>
2020-11-06 14:07:05 -0500

    Pulls More Data
    Fetch is now in a loop to pull more data.

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const FORMAT = "format=json";
 const OFFSET = "offset=";
 
 var url = "https://www.giantbomb.com/api/user_reviews/?";
-var offsetVal = 0;
 var settings = { method: "Get" };
 var entries = [];
 
@@ -15,39 +14,56 @@ function checkValid(json, key) {
   return false;
 }
 
-fetch(url + APIKEY + "&" + FORMAT + "&" + OFFSET + offsetVal, settings)
-  .then((res) => res.json())
-  .then((json) => {
-    json.results.forEach((review) => {
-      entries.push({
-        user: review.hasOwnProperty("reviewer") ? review.reviewer : "",
-        gameID: checkValid(review.game, "id") ? review.game.id : "",
-        gameURL: checkValid(review.game, "site_detail_url")
-          ? review.game.site_detail_url
-          : "",
-        gameName: checkValid(review.game, "name") ? review.game.name : "",
-        score: review.hasOwnProperty("score") ? review.score : "",
-        dateAdded: review.hasOwnProperty("date_added") ? review.date_added : "",
-        dateUpdated: review.hasOwnProperty("date_last_updated")
-          ? review.date_last_updated
-          : "",
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function getDatasets() {
+  for (var i = 0; i < 1000; i++) {
+    fetch(url + APIKEY + "&" + FORMAT + "&" + OFFSET + (i * 100), settings)
+      .then((res) => res.json())
+      .then((json) => {
+        json.results.forEach((review) => {
+          entries.push({
+            user: review.hasOwnProperty("reviewer") ? review.reviewer : "",
+            gameID: checkValid(review.game, "id") ? review.game.id : "",
+            gameURL: checkValid(review.game, "site_detail_url")
+              ? review.game.site_detail_url
+              : "",
+            gameName: checkValid(review.game, "name") ? review.game.name : "",
+            score: review.hasOwnProperty("score") ? review.score : "",
+            dateAdded: review.hasOwnProperty("date_added")
+              ? review.date_added
+              : "",
+            dateUpdated: review.hasOwnProperty("date_last_updated")
+              ? review.date_last_updated
+              : "",
+          });
+        });
+
+        var csvWriter = createCsvWriter.createObjectCsvWriter({
+          path: "./export/reviewData" + i + ".csv",
+          header: [
+            { id: "user", title: "user" },
+            { id: "gameID", title: "game_id" },
+            { id: "gameURL", title: "game_url" },
+            { id: "gameName", title: "game_name" },
+            { id: "score", title: "score" },
+            { id: "dateAdded", title: "date_added" },
+            { id: "dateUpdated", title: "date_updated" },
+          ],
+        });
+
+        csvWriter.writeRecords(entries).then(() => {
+          console.log("Dataset " + i + " complete!");
+        });
       });
-    });
 
-    var csvWriter = createCsvWriter.createObjectCsvWriter({
-      path: "./export/reviewData" + offsetVal + ".csv",
-      header: [
-        { id: "user", title: "user" },
-        { id: "gameID", title: "game_id" },
-        { id: "gameURL", title: "game_url" },
-        { id: "gameName", title: "game_name" },
-        { id: "score", title: "score" },
-        { id: "dateAdded", title: "date_added" },
-        { id: "dateUpdated", title: "date_updated" },
-      ],
-    });
+    await sleep(2000);
+  }
+}
 
-    csvWriter.writeRecords(entries).then(() => {
-      console.log("...Done");
-    });
-  });
+
+getDatasets();
+
+
